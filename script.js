a6a88b769092d6491cd8f0501d5057b795c4e019
makenasandra <makenawachira@gmail.com>
2025-04-09 20:56:10 +0300

    modify

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 const generateTarget = ()=>{
-  return Math.ceil(Math.random()*9);
+  return Math.ceil(Math.random()*10);
 }
 
 const compareGuesses = (userGuess, compGuess, target)=>{
+    if (userGuess < 0 || userGuess > 9) {
+        alert('Your guess is out of range! Please enter a number between 0 and 9.');
+        return;
+      }
   const diff = Math.abs(target-compGuess);
   const diff2 = Math.abs(target-userGuess);
   if(diff<diff2){
@@ -19,12 +23,12 @@ const compareGuesses = (userGuess, compGuess, target)=>{
 
 const updateScore = (string)=>{
 if(string==="human" ){
-  humanScore ++;
+  humanScore++;
 }else{
   computerScore++;
 }
 }
 
 const advanceRound = ()=>{
-  currentRoundNumber ++;
+  currentRoundNumber++;
 }
